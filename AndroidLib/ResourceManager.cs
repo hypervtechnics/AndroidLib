ba3a15fc5bb99cc9a9cfc67ed21dd6a1cac8244b
unknown <ab@c.de>
2015-09-19 15:48:20 +0200

    Initial commit

---
+++ AndroidLib/ResourceManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidLib
+{
+    public class ResourceManager
+    {
+        public static string adbPrefix = "adb"; //Wenn in Windows Verzeichnis vorhanden andernfalls vollständiger Dateipfad mit Dateiname
+    }
+}
