da0cf30fcda1fe83a966cbe145583683a9943eae
Hyper V Technics <hvt>
2015-09-19 17:11:48 +0200

    Added TODO

---
+++ AndroidLib/Classes/Adb/Adb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidLib.Adb
+{
+    public class Adb
+    {
+        public static String ExecuteAdbCommandWithOutput(String command, Device device = null)
+        {
+            //TODO
+            return "";
+        }
+    }
+}
