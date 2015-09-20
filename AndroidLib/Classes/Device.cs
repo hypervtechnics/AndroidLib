9cf7a30f5fdee8f7b93246d022953fa94e9f14de
Hyper V Technics <hvt>
2015-09-20 16:55:44 +0200

    Updated class

--- AndroidLib/Classes/Device.cs
+++ AndroidLib/Classes/Device.cs
@@ -14,13 +14,20 @@ namespace AndroidLib
         /// Creates a device with the specific serial number
         /// </summary>
         /// <param name="serialNo">The serial number of the device as a String</param>
-        public Device(String serialNo, String model, String productName)
+        /// <param name="model">The model of the device</param>
+        /// <param name="productName">The product name of the device</param>
+        /// <param name="state">The state of the device</param>
+        public Device(String serialNo, String model, String productName, String name, DeviceState state)
         {
             mSerialNumber = serialNo;
             mProductName = productName;
             mModel = model;
+            mConnectionStatus = state;
+            mName = name;
             this.updateInfo();
         }
+
+
 
         #endregion
 
@@ -29,6 +36,8 @@ namespace AndroidLib
         private String mSerialNumber;
         private String mModel;
         private String mProductName;
+        private DeviceState mConnectionStatus;
+        private String mName;
 
         #endregion
 
@@ -68,6 +77,25 @@ namespace AndroidLib
             }
         }
 
+        public String Name
+        {
+            get
+            {
+                return mName;
+            }
+        }
+
+        /// <summary>
+        /// Returns the state of the device
+        /// </summary>
+        public DeviceState ConnectionStatus
+        {
+            get
+            {
+                return mConnectionStatus;
+            }
+        }
+
         #endregion
 
         #region Private Methods
@@ -75,7 +103,6 @@ namespace AndroidLib
         internal void updateInfo()
         {
             //TODO
-
         }
 
         #endregion
@@ -92,4 +119,15 @@ namespace AndroidLib
 
         #endregion
     }
+
+    #region State of devices
+    public enum DeviceState
+    {
+        Unknown,
+        Online,
+        Offline,
+        Bootloader,
+        Unauthorized
+    }
+    #endregion
 }
