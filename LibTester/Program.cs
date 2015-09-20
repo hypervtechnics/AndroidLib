fdabbc91ccbf11ab1070c7b9330670214968f93e
Hyper V Technics <hvt>
2015-09-20 14:45:34 +0200

    Added Tester Project

---
+++ LibTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using AndroidLib.Adb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Adb.ExecuteAdbCommandWithOutput("devices", null, false));
+            Console.Write("FINISHED!");
+            Console.Read();
+        }
+    }
+}
