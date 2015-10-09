9466913172840ad1928463f1ce892a45722f64aa
Hyper V Technics <hvt>
2015-10-09 20:53:57 +0200

    Added new extension methods

--- AndroidLib/Internal Classes/ExtensionMethods.cs
+++ AndroidLib/Internal Classes/ExtensionMethods.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 namespace AndroidLib
 {
     //Taken from https://github.com/regaw-leinad/AndroidLib
-
     internal static class ExtensionMethods
     {
         internal static bool ContainsIgnoreCase(this string s, string str)
@@ -34,6 +33,64 @@ namespace AndroidLib
                 }
             }
             return final;
+        }
+    }
+
+    //Taken from http://www.dotnetperls.com/between-before-after
+    static class SubstringExtensions
+    {
+        /// <summary>
+        /// Get string value between [first] a and [last] b.
+        /// </summary>
+        public static string Between(this string value, string a, string b)
+        {
+            int posA = value.IndexOf(a);
+            int posB = value.LastIndexOf(b);
+            if (posA == -1)
+            {
+                return "";
+            }
+            if (posB == -1)
+            {
+                return "";
+            }
+            int adjustedPosA = posA + a.Length;
+            if (adjustedPosA >= posB)
+            {
+                return "";
+            }
+            return value.Substring(adjustedPosA, posB - adjustedPosA);
+        }
+
+        /// <summary>
+        /// Get string value after [first] a.
+        /// </summary>
+        public static string Before(this string value, string a)
+        {
+            int posA = value.IndexOf(a);
+            if (posA == -1)
+            {
+                return "";
+            }
+            return value.Substring(0, posA);
+        }
+
+        /// <summary>
+        /// Get string value after [last] a.
+        /// </summary>
+        public static string After(this string value, string a)
+        {
+            int posA = value.LastIndexOf(a);
+            if (posA == -1)
+            {
+                return "";
+            }
+            int adjustedPosA = posA + a.Length;
+            if (adjustedPosA >= value.Length)
+            {
+                return "";
+            }
+            return value.Substring(adjustedPosA);
         }
     }
 }
