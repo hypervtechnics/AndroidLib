37ed6c5cc6d35b07d4982eb642fa13aa7bea8c71
Hyper V Technics <hvt>
2015-10-15 22:57:50 +0200

    Added RunCommand method but not working properly

--- AndroidLib/Classes/Base/Shell.cs
+++ AndroidLib/Classes/Base/Shell.cs
@@ -26,6 +26,7 @@ namespace AndroidLib.Base
             mProcess.StartInfo.CreateNoWindow = true;
             mProcess.StartInfo.UseShellExecute = false;
             mProcess.StartInfo.RedirectStandardInput = true;
+            mProcess.StartInfo.RedirectStandardOutput = true;
             mProcess.Start();
         }
 
@@ -62,6 +63,56 @@ namespace AndroidLib.Base
             }
         }
 
+        /// <summary>
+        /// Executes the command given and returns it output
+        /// </summary>
+        /// <param name="command">The command to execute</param>
+        /// <returns></returns>
+        public List<string> RunCommand(string command)
+        {
+            //Ini output
+            List<string> output = new List<string>();
+
+            //Execute command
+            mProcess.StandardInput.WriteLine(command);
+
+            //Throw away first line
+            mProcess.StandardOutput.ReadLine();
+
+            //Then get first line of real output
+            string lastLine = mProcess.StandardOutput.ReadLine().Trim();
+
+            //And check whether its output or the output already exited
+            while (!lastLine.Contains("@android:/"))
+            {
+                //Build line by myself because when shell exits script it doesnt write a line terminator and this will lead to an infinite loop
+                //{
+                //    string tempString = "";
+                //    Boolean isLine = false;
+
+                //    while(!isLine)
+                //    {
+                //        //Check if deadly line is there or if its the end of a line
+                //        if (tempString.Contains("@android:/") || tempString.EndsWith("\r") || tempString.EndsWith("\n")) isLine = true;
+                //        else tempString += mProcess.StandardOutput.Read();
+                //    }
+
+                //    lastLine = tempString.Trim();
+                //}
+
+                //Not optimal at the moment (TODO)
+                lastLine = mProcess.StandardOutput.ReadLine().Trim();
+
+                //If its okay add it to output
+                if (!string.IsNullOrWhiteSpace(lastLine)) output.Add(lastLine);
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Frees all resources needed by shell
+        /// </summary>
         public void Dispose()
         {
             if(!mProcess.HasExited) mProcess.Kill();
