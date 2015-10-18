fc5f2433453e8fd10bc145b1d0e3cd933d025308
Hyper V Technics <hvt>
2015-10-18 18:43:10 +0200

    Introduced and halfly implemented Backup and Restore

---
+++ AndroidLib/Classes/Wrapper/BaR/Backup.cs
@@ -0,0 +1,116 @@
+﻿using AndroidLib.Base;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AndroidLib.Wrapper
+{
+    public class Backup : IDisposable
+    {
+        private long mLastSize;
+        private Device mDevice;
+        private Process mProcess;
+        private Thread mThread;
+        private string mFilename;
+
+        public event EventHandler<OnBackupCompletedArgs> OnBackupCompleted;
+        public event EventHandler<OnBackupProgressChangedArgs> OnBackupProgressChanged;
+
+        public class OnBackupCompletedArgs : EventArgs
+        {
+            public TimeSpan Time;
+            public long Size;
+            public string Output;
+
+            public OnBackupCompletedArgs(TimeSpan time, long size, string output)
+            {
+                Time = time;
+                Size = size;
+                Output = output;
+            }
+        }
+
+        public class OnBackupProgressChangedArgs : EventArgs
+        {
+            public bool WaitingForUserInput;
+            public long Size;
+
+            public OnBackupProgressChangedArgs(bool waiting, long size)
+            {
+                WaitingForUserInput = waiting;
+                Size = size;
+            }
+        }
+
+        internal Backup(string command, Device device, string filename)
+        {
+            mProcess = new Process();
+            mProcess.StartInfo.FileName = ResourceManager.adbPrefix;
+            mProcess.StartInfo.Arguments = "-s " + device.SerialNumber + " " + command;
+            mProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            mProcess.StartInfo.CreateNoWindow = true;
+            mProcess.StartInfo.UseShellExecute = false;
+            mProcess.StartInfo.RedirectStandardOutput = true;
+
+            mLastSize = 0L;
+            mDevice = device;
+            mFilename = filename;
+
+            mThread = new Thread(() =>
+            {
+                bool waitingAlreadyRaised = false;
+
+                while(!mProcess.HasExited)
+                {
+                    FileInfo file = new FileInfo(mFilename);
+
+                    if(file.Exists)
+                    {
+                        long currentSize = file.Length;
+
+                        if(currentSize == 0L && !waitingAlreadyRaised)
+                        {
+                            waitingAlreadyRaised = true;
+                            if (OnBackupProgressChanged != null) OnBackupProgressChanged(this, new OnBackupProgressChangedArgs(true, mLastSize));
+                        }
+
+                        if(currentSize > mLastSize)
+                        {
+                            if (OnBackupProgressChanged != null) OnBackupProgressChanged(this, new OnBackupProgressChangedArgs(false, mLastSize));
+                        }
+
+                        mLastSize = currentSize;
+                    }
+
+                    Thread.Sleep(3000);
+                }
+
+                if (OnBackupCompleted != null) OnBackupCompleted(this, new OnBackupCompletedArgs(mProcess.ExitTime - mProcess.StartTime, new FileInfo(mFilename).Length, mProcess.StandardOutput.ReadToEnd()));
+            })
+            { IsBackground = true };
+        }
+
+        public void Start()
+        {
+            mProcess.Start();
+            mThread.Start();
+        }
+
+        public void Dispose()
+        {
+            mThread.Abort();
+            mThread = null;
+
+            if (!mProcess.HasExited)
+            {
+                mProcess.Kill();
+                mProcess.Close();
+            }
+        }
+    }
+}
---
+++ AndroidLib/Classes/Wrapper/BaR/BackupRestoreManager.cs
@@ -0,0 +1,62 @@
+﻿using AndroidLib.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidLib.Wrapper
+{
+    public class BackupRestoreManager
+    {
+        private Device mDevice;
+
+        internal BackupRestoreManager(Device device)
+        {
+            mDevice = device;
+        }
+
+        /// <summary>
+        /// Creates an Backup instance which manages the backup process
+        /// </summary>
+        /// <param name="filename">The filename of the file the backup will be saved to</param>
+        /// <param name="includeApk">Whether the associated apk file of an app should be saved</param>
+        /// <param name="includeObb">Whether to backup the associated apk expansion file of the app</param>
+        /// <param name="includeInternal">Whether to include sd card contents</param>
+        /// <param name="backupAll">Whether to backup all applications</param>
+        /// <param name="includeSystemApps">Whether to include system apps too</param>
+        /// <param name="asyncProcess">Whether to block thread until backup is completed</param>
+        /// <param name="packages">Optionally the list of packages to backup (Note: backupAll has to be false)</param>
+        /// <returns>An object handling the Backup process</returns>
+        public Backup MakeBackup(string filename, bool includeApk, bool includeObb, bool includeInternal, bool backupAll, bool includeSystemApps, bool asyncProcess, List<string> packages = null)
+        {
+            //Build cmd string
+            string command = "backup -f \"" + filename + "\" ";
+
+            if (includeApk) command += "-apk ";
+            else command += "-noapk ";
+
+            if (includeObb) command += "-obb ";
+            else command += "-noobb";
+
+            if (includeInternal) command += "-shared ";
+            else command += "-noshared ";
+
+            if (backupAll) command += "-all ";
+
+            if (includeSystemApps) command += "-system";
+            else command += "-nosystem";
+
+            if(packages != null && !backupAll)
+            {
+                foreach(string packagename in packages)
+                {
+                    command += " " + packagename;
+                }
+            }
+
+            //Create object and block if neccessary
+            return new Backup(command, mDevice, filename);
+        }
+    }
+}
